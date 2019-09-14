ab98066705cecae835ea66d20e66ee653ff87b76
Roman Fedorov <studentutu@gmail.com>
2019-09-14 19:46:53 +0300

    added landing page, setup MVC, need to add Mongo DB (NoSQL),

---
+++ SetupMVC/Controllers/PeopleController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SetupMVC.Models;
+
+namespace SetupMVC.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            var any = new PersonModel { Age = 0, UserName = "Studentutu", LastName = "Fedorov" };
+            int i = 1 / any.Age;
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            people.Add(new PersonModel { Age = 24, UserName = "Studentutu", LastName = "Fedorov"});
+            people.Add(new PersonModel { Age = 38, UserName = "Joe", LastName = "Cow" });
+            people.Add(new PersonModel { Age = 54, UserName = "Steve", LastName = "Split" });
+
+            return View(people);
+        }
+    }
+}
---
+++ SetupMVC/Models/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SetupMVC.Models
+{
+    public class PersonModel
+    {
+        public string UserName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; } = 0;
+        public bool IsAliva { get; set; } = true;
+    }
+}
